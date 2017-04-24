3a37188d1ca9d5716e995ed6b37bf563c22b69fe
simonchan22 <jxchen@bu.edu>
2017-04-24 09:26:41 -0400

    script is wrong
    script is wrong

--- flashLED.py
+++ flashLED.py
@@ -1,7 +1,7 @@
 import sys
 import wiringpi2 as wpi
 from time import sleep
-pirnt "Starting the Python wiringPi example"
+print "Starting the Python wiringPi example"
 wpi.wiringPiSetupGpio()
 wpi.pinMode(17,1)
 wpi.pinMode(27,0)
