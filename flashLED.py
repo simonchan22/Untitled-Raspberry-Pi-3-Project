580bfb79a4ecda5d3068538b96b1af61dfb624dd
simonchan22 <jxchen@bu.edu>
2017-04-24 09:26:01 -0400

    The script is wrong

--- flashLED.py
+++ flashLED.py
@@ -5,9 +5,7 @@ pirnt "Starting the Python wiringPi example"
 wpi.wiringPiSetupGpio()
 wpi.pinMode(17,1)
 wpi.pinMode(27,0)
-ledout = wpi.digitalRead(17)
-button = wpi.digitalRead(27)
-while buttonout == 0:
+while wpi.digitalRead(27) == 0:
 	wpi.digitalWrite(17,1)
 	sleep(0.5)
 	wpi.digitalWrite(17,1)
