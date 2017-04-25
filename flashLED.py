202e97e85a9a1232c343ef033518ba9feb5b1cfb
simonchan22 <jxchen@bu.edu>
2017-04-24 21:56:58 -0400

    Bug Fixed

--- flashLED.py
+++ flashLED.py
@@ -1,5 +1,5 @@
 import sys
-import wiringpi2 as wpi
+import wiringpi as wpi
 from time import sleep
 print "Starting the Python wiringPi example"
 wpi.wiringPiSetupGpio()
@@ -8,7 +8,7 @@ wpi.pinMode(27,0)
 while wpi.digitalRead(27) == 0:
 	wpi.digitalWrite(17,1)
 	sleep(0.5)
-	wpi.digitalWrite(17,1)
+	wpi.digitalWrite(17,0)
 	sleep(0.5)
 print ("Button pressed to end the example")
 wpi.digitalWrite(17,0)
